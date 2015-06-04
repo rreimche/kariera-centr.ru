a57ee1734ff4533fce6a4fa733c12e437ab95c8c
Roman Reimche <rreimche@Roman-Reimches-MacBook.local>
2015-06-04 18:45:16 +0200

    Some content from the homepages was moved to the sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :prepare_sidebar
   layout Proc.new{
   	if controller_name == "control_panel" or ['edit', 'new'].include?(action_name)
   		'edit'
   	else
   		'application'
   	end
-  }
+  } 
+
+  def prepare_sidebar
+    @news = News.order(created_at: :desc).limit(7)
+    @courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date)
+  end
 end
--- app/controllers/homepage_controller_controller.rb
+++ app/controllers/homepage_controller_controller.rb
@@ -1,6 +1,6 @@
 class HomepageControllerController < ApplicationController
   def show
-  	@news = News.order(created_at: :desc).limit(7)
-  	@courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date)
+  	#@news = News.order(created_at: :desc).limit(7)
+  	#@courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date)
   end
 end
