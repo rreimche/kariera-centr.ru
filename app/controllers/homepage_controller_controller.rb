8265d70abe1ff6d0d13ed497e293dbcea4bec0be
Roman Reimche <roman.reimche@gmail.com>
2015-09-14 11:12:00 +0200

    minor changes to actual courses

--- app/controllers/homepage_controller_controller.rb
+++ app/controllers/homepage_controller_controller.rb
@@ -4,5 +4,6 @@ class HomepageControllerController < ApplicationController
   	#@courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date)
   	@num_courses = Course.count
   	@hot_offers = HotOffer.order(created_at: :desc).limit(7)
+  	@courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date).limit(6)
   end
 end
