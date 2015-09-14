6f45c0b42c9ac6b4e3425540678d3863329bf175
Roman Reimche <roman.reimche@gmail.com>
2015-09-14 05:39:46 +0200

    Basic things done

--- app/controllers/homepage_controller_controller.rb
+++ app/controllers/homepage_controller_controller.rb
@@ -2,5 +2,6 @@ class HomepageControllerController < ApplicationController
   def show
   	#@news = News.order(created_at: :desc).limit(7)
   	#@courses_next = Course.where(start_date: (Time.now.midnight - 7.days)..(Time.now.midnight+1.month)).order(:start_date)
+  	@num_courses = Course.count
   end
 end
