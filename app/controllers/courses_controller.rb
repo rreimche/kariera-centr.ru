c5ff2d206e1f5400d0175c44d54d715a2374b632
Roman Reimche <roman.reimche@gmail.com>
2015-10-06 13:29:45 +0200

    integrated feedbacks into courses (use [отзывы] shortcut) and into homepage

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,6 +24,11 @@ class CoursesController < ApplicationController
   # GET /courses/1
   # GET /courses/1.json
   def show
+    feedbacks = Feedback.where(course: @course)
+    if feedbacks.size != 0
+      feedbacks = render_to_string partial: 'feedbacks', locals: {feedbacks: feedbacks}
+      @course.full_descr.sub! '[отзывы]', feedbacks
+    end
   end
 
   # GET /courses/new
