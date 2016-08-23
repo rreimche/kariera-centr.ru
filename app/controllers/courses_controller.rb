9f6cf1560e4f337677bab34ab8b8a04f9b296955
Roman Reimche <roman.reimche@gmail.com>
2016-08-23 20:20:39 +0200

    fixed feedbacks shortcode for panels

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -53,6 +53,9 @@ class CoursesController < ApplicationController
     if feedbacks.size != 0
       feedbacks = render_to_string partial: 'feedbacks', locals: {feedbacks: feedbacks, feedbacks_are_own: feedbacks_are_own}
       @course.full_descr.sub! '[отзывы]', feedbacks
+      (0...ENV['COURSE_PANELS_QTY'].to_i).each do |i|
+        @course["panel#{i}_content".to_sym].sub! '[отзывы]', feedbacks
+      end
     end
 
     begin 
